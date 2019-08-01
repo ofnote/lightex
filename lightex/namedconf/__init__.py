cee7757571f77d8cd26e05fc38a5b4bd1572dbec
Nishant Sinha <nishant@offnote.co>
2019-08-01 12:13:26 +0530

    added argparse_to_command

--- lightex/namedconf/__init__.py
+++ lightex/namedconf/__init__.py
@@ -3,4 +3,5 @@
 
 from .config_utils import to_dict, to_yaml, render_command, unroll_top_fields
 from .config_utils import load_config, update_dataconfig_with_args
+from .config_utils import argparse_to_command
 from .replace_utils import rreplace
--- lightex/namedconf/config_utils.py
+++ lightex/namedconf/config_utils.py
@@ -1,3 +1,4 @@
+import sys
 from typing import List
 from jinja2 import Template, DebugUndefined
 from dataclasses import dataclass, asdict, make_dataclass, field, replace
@@ -7,6 +8,8 @@ from dacite import from_dict
 
 def to_dict(dc):
     return ED(asdict(dc))
+
+
 
 def render_command(expt):
     if not isinstance(expt, dict):
@@ -90,7 +93,78 @@ def to_yaml (C):
     d = asdict(C)
     return yaml.dump(d)
 
+def argparse_to_command (args, add_placeholders=True, values_are_names=True):
+    #print (vars(args))
+    res = [sys.argv[0]]
+    for k, v in vars(args).items():
+        val = f'{k}' if values_are_names else f'{v}'
+        if add_placeholders:
+            val = '{{' + val + '}}'
+        res.append(f'--{k} ' + val)
+    return ' '.join(res)
 
+
+def test_argparse_to_command ():
+    import argparse
+    parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
+    parser.add_argument('data', metavar='DIR',
+                        help='path to dataset')
+    parser.add_argument('--output-dir', help='path to model output dir')
+
+    parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet18',
+                        help='model architecture (default: resnet18)')
+    parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
+                        help='number of data loading workers (default: 4)')
+    parser.add_argument('--epochs', default=90, type=int, metavar='N',
+                        help='number of total epochs to run')
+    parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
+                        help='manual epoch number (useful on restarts)')
+    parser.add_argument('-b', '--batch-size', default=256, type=int,
+                        metavar='N',
+                        help='mini-batch size (default: 256), this is the total '
+                             'batch size of all GPUs on the current node when '
+                             'using Data Parallel or Distributed Data Parallel')
+    parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
+                        metavar='LR', help='initial learning rate', dest='lr')
+    parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                        help='momentum')
+    parser.add_argument('--wd', '--weight-decay', default=1e-4, type=float,
+                        metavar='W', help='weight decay (default: 1e-4)',
+                        dest='weight_decay')
+    parser.add_argument('-p', '--print-freq', default=10, type=int,
+                        metavar='N', help='print frequency (default: 10)')
+    parser.add_argument('--resume', default='', type=str, metavar='PATH',
+                        help='path to latest checkpoint (default: none)')
+    parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
+                        help='evaluate model on validation set')
+    parser.add_argument('--pretrained', dest='pretrained', action='store_true',
+                        help='use pre-trained model')
+    parser.add_argument('--world-size', default=-1, type=int,
+                        help='number of nodes for distributed training')
+    parser.add_argument('--rank', default=-1, type=int,
+                        help='node rank for distributed training')
+    parser.add_argument('--dist-url', default='tcp://224.66.41.62:23456', type=str,
+                        help='url used to set up distributed training')
+    parser.add_argument('--dist-backend', default='nccl', type=str,
+                        help='distributed backend')
+    parser.add_argument('--seed', default=None, type=int,
+                        help='seed for initializing training. ')
+    parser.add_argument('--gpu', default=None, type=int,
+                        help='GPU id to use.')
+    parser.add_argument('--image_size', default=224, type=int,
+                        help='image size')
+    parser.add_argument('--multiprocessing-distributed', action='store_true',
+                        help='Use multi-processing distributed training to launch '
+                             'N processes per node, which has N GPUs. This is the '
+                             'fastest way to use PyTorch for either single node or '
+                             'multi node data parallel training')
+
+    args = parser.parse_args()
+    cmd = argparse_to_command(args)
+    print ('command = ', cmd)
+
+if __name__ == '__main__':
+    test_argparse_to_command()
 
 '''
 
