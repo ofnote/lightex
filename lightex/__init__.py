f5060397b3daca5f92c18b87c8197689144a3879
Nishant Sinha <nishant@offnote.co>
2019-08-09 16:42:46 +0530

    (qviz) display mlflow run stats

--- lightex/__init__.py
+++ lightex/__init__.py
@@ -1,3 +1,2 @@
 from .dispatch import dispatch_expts
 from . import config_base as config
-#from .namedconf import load_config, flatten_all_top_fields, to_yaml, to_dict
---
+++ lightex/qviz/show.py
@@ -0,0 +1,53 @@
+from ..mulogger.config import LoggerConfig, MLFlowConfig
+from dataclasses import dataclass
+from easydict import EasyDict as ED
+import pandas as pd
+
+class MLflowQViz:
+    def __init__(self, config=MLFlowConfig()):
+        import mlflow
+        self.mlflow = mlflow
+        self.client = mlflow.tracking.MlflowClient(config.uri())
+        #from mlflow.entities import RunInfo, RunStatus
+
+    def show_runs(self, runs, meta_fields=['status', 'start_time', 'end_time', 'run_id']):
+        df = pd.DataFrame([{fn: getattr(run.info, fn) for fn in meta_fields} for run in runs], columns=meta_fields)
+        val = df['end_time'] - df['start_time']
+        df.insert(0, 'run_time', val)
+        #print (df)
+
+        for run in runs:
+            run_id = run.info.run_id
+            print (f'** run {run_id}')
+            print (df[df['run_id'] == run_id])
+
+            data = run.data
+            print (f'params: {data.params}')
+            print (f'metrics: {data.metrics}')
+
+    def get_all_runs(self):
+        expts = self.client.list_experiments()
+        expts = [(expt.experiment_id, expt.name) for expt in expts if expt.lifecycle_stage == 'active']
+
+        exptid2runs = {}
+        for eid, name in expts:
+            runinfos = self.client.list_run_infos(eid)
+            runs = [self.client.get_run(ri.run_id) for ri in runinfos]
+            exptid2runs[eid] = ED({'name': name, 'runs': runs})
+
+        return exptid2runs
+
+    def show_expt_runs(self):
+        exptid2runs = self.get_all_runs()
+        for eid, val in exptid2runs.items():
+            print (f'\n==== {val.name} =====>')
+            self.show_runs(val.runs)
+
+
+def show_all_runs(config=LoggerConfig(loggers=['mlflow'])):
+    mq = MLflowQViz(config.mlflow)
+    mq.show_expt_runs()
+
+if __name__ == '__main__':
+    show_all_runs()
+    
